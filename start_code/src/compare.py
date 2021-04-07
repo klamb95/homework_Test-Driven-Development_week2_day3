78d7367a0a3b82467db953a175e642ac28a87149
klamb95 <klamb1995@gmail.com>
2021-04-07 16:45:29 +0100

    finished homework

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,2 +1,6 @@
-def compare():
-    pass
+def compare(num_1, num_2):
+    if num_1 == num_2:
+        return "Both numbers are the same"
+    if num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+    return f"{num_1} is greater than {num_2}"
